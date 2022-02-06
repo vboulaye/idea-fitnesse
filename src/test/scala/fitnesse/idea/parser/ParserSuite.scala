ea15a908af1c867410fbf806188ef1953ac76a92
Meelis Lehtmets <meelis@lehtmets.com>
2022-02-06 18:51:47 +0200

    Compile using IDEA 2017.3

--- src/test/scala/fitnesse/idea/parser/ParserSuite.scala
+++ src/test/scala/fitnesse/idea/parser/ParserSuite.scala
@@ -1,5 +1,6 @@
 package fitnesse.idea.parser
 
+import com.intellij.core.CoreApplicationEnvironment
 import com.intellij.lang._
 import com.intellij.lang.impl.PsiBuilderFactoryImpl
 import com.intellij.lang.injection.MultiHostInjector
@@ -13,11 +14,14 @@ import com.intellij.openapi.fileEditor.impl.FileDocumentManagerImpl
 import com.intellij.openapi.fileTypes.FileTypeManager
 import com.intellij.openapi.progress.ProgressManager
 import com.intellij.openapi.progress.impl.ProgressManagerImpl
+import com.intellij.openapi.roots.ProjectFileIndex
+import com.intellij.openapi.roots.impl.{DirectoryIndexImpl, ProjectFileIndexImpl}
+import com.intellij.psi.impl.smartPointers.SmartPointerManagerImpl
 import com.intellij.psi.impl.source.SourceTreeToPsiMap
 import com.intellij.psi.impl.source.tree.{FileElement, CompositeElement}
 import com.intellij.psi.impl.{PsiFileFactoryImpl, PsiManagerEx}
 import com.intellij.psi.tree.IElementType
-import com.intellij.psi.{PsiFileFactory, SingleRootFileViewProvider}
+import com.intellij.psi.{PsiFileFactory, PsiManager, SingleRootFileViewProvider, SmartPointerManager}
 import com.intellij.testFramework.LightVirtualFile
 import com.intellij.util.Function
 import fitnesse.idea.filetype.FitnesseLanguage
@@ -26,7 +30,7 @@ import org.scalatest.{BeforeAndAfterAll, FunSuite, Matchers}
 
 trait ParserSuite extends FunSuite with Matchers with BeforeAndAfterAll {
   val parserDefinition = new FitnesseParserDefinition
-  val myTestRootDisposable = new Disposable {
+  val myTestRootDisposable: Disposable = new Disposable {
     def dispose(): Unit = {}
   }
 
@@ -52,14 +56,23 @@ trait ParserSuite extends FunSuite with Matchers with BeforeAndAfterAll {
     Extensions.registerAreaClass("IDEA_PROJECT", null)
     Extensions.registerAreaClass(MultiHostInjector.MULTIHOST_INJECTOR_EP_NAME.getName, null)
 
+    CoreApplicationEnvironment.registerApplicationExtensionPoint(MetaLanguage.EP_NAME, classOf[MetaLanguage])
+
     app = new MockApplicationEx(myTestRootDisposable)
     myProject = new MockProjectEx(myTestRootDisposable)
     myPsiManager = new MockPsiManager(myProject)
     myPsiFileFactory = new PsiFileFactoryImpl(myPsiManager)
 
     myProject.getPicoContainer.registerComponentInstance(classOf[PsiFileFactory].getName, myPsiFileFactory)
+    myProject.getPicoContainer.registerComponentInstance(classOf[PsiManager].getName, myPsiManager)
 
     ApplicationManager.setApplication(app, myTestRootDisposable)
+
+    val projectFileIndex = new ProjectFileIndexImpl(
+      myProject,
+      new DirectoryIndexImpl(myProject),
+      new com.intellij.openapi.fileTypes.MockFileTypeManager
+    )
 
     app.getPicoContainer.registerComponentInstance(classOf[FileTypeManager].getName, new MockFileTypeManager(new MockLanguageFileType(parserDefinition.getFileNodeType.getLanguage, "txt")))
     app.getPicoContainer.registerComponentInstance(classOf[EditorFactory].getName, editorFactory)
@@ -67,6 +80,8 @@ trait ParserSuite extends FunSuite with Matchers with BeforeAndAfterAll {
     app.getPicoContainer.registerComponentInstance(classOf[DefaultASTFactory].getName, new DefaultASTFactoryImpl)
     app.getPicoContainer.registerComponentInstance(classOf[PsiBuilderFactory].getName, new PsiBuilderFactoryImpl)
     app.getPicoContainer.registerComponentInstance(classOf[ProgressManager].getName,  new ProgressManagerImpl)
+    app.getPicoContainer.registerComponentInstance(classOf[ProjectFileIndex].getName,  projectFileIndex)
+    app.getPicoContainer.registerComponentInstance(classOf[SmartPointerManager].getName,  new SmartPointerManagerImpl(myProject))
 
     LanguageParserDefinitions.INSTANCE.addExplicitExtension(parserDefinition.getFileNodeType.getLanguage, parserDefinition)
   }
@@ -79,8 +94,11 @@ trait ParserSuite extends FunSuite with Matchers with BeforeAndAfterAll {
     app.getPicoContainer.unregisterComponent(classOf[DefaultASTFactory].getName)
     app.getPicoContainer.unregisterComponent(classOf[PsiBuilderFactory].getName)
     app.getPicoContainer.unregisterComponent(classOf[ProgressManager].getName)
+    app.getPicoContainer.unregisterComponent(classOf[ProjectFileIndex].getName)
+    app.getPicoContainer.unregisterComponent(classOf[SmartPointerManager].getName)
 
     myProject.getPicoContainer.unregisterComponent(classOf[PsiFileFactory].getName)
+    myProject.getPicoContainer.unregisterComponent(classOf[PsiManager].getName)
   }
 
   abstract class Tree
